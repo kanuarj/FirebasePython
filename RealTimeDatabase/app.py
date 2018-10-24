420cd5a15495eed5f2fb50f6a2118dd9545b2430
kanuarj <jimmy.lpf@gmail.com>
2018-10-24 15:37:45 +0530

    rtdb

---
+++ RealTimeDatabase/app.py
@@ -0,0 +1,38 @@
+import pyrebase
+
+config = {
+	"apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def basic():
+	if request.method == 'POST':
+		if request.form['submit'] == 'add':
+
+			name = request.form['name']
+			db.child("todo").push(name)
+			todo = db.child("todo").get()
+			to = todo.val()
+			return render_template('index.html', t=to.values())
+		elif request.form['submit'] == 'delete':
+			db.child("todo").remove()
+		return render_template('index.html')
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
